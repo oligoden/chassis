2137b74ff92fc785028e62bfd3fdae5fe5670d22
Anro le Roux <anro@oligoden.com>
2021-01-23 22:28:54 +0200

    Add DELETE method

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -13,12 +13,29 @@ func (a Adapter) Entry() http.Handler {
 	return a.Handler
 }
 
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
 func (a Adapter) Notify() Adapter {
 	return Adapter{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Printf("--> request %s %s from %s\n\t%s\n\t%s\n", r.Method, r.URL.String(), r.RemoteAddr, r.UserAgent(), r.Header.Get("ax_session_user"))
-			a.Handler.ServeHTTP(w, r)
-			fmt.Printf("<-- response %s\n\n", w.Header().Get("X_Session_User"))
+			fmt.Printf("--> request %s %s from %s\n--  %s\n--  user %s, session %s\n", r.Method, r.URL.String(), r.RemoteAddr, r.UserAgent(), r.Header.Get("X_user"), r.Header.Get("X_session"))
+
+			lw := NewLoggingResponseWriter(w)
+			a.Handler.ServeHTTP(lw, r)
+
+			fmt.Printf("<-- response %d %s %s\n\n", lw.statusCode, w.Header().Get("X_user"), w.Header().Get("X_session"))
 		}),
 	}
 }
@@ -60,6 +77,18 @@ func (a Adapter) Put(h http.Handler) Adapter {
 	return Adapter{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if r.Method == http.MethodPut {
+				h.ServeHTTP(w, r)
+			} else {
+				a.Handler.ServeHTTP(w, r)
+			}
+		}),
+	}
+}
+
+func (a Adapter) Delete(h http.Handler) Adapter {
+	return Adapter{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodDelete {
 				h.ServeHTTP(w, r)
 			} else {
 				a.Handler.ServeHTTP(w, r)
