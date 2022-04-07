626b108a4aaca17592120cb856b8d62b9a1b4119
Anro le Roux <anro@oligoden.com>
2022-04-07 18:28:48 +0200

    Add serveTLS method to mux

--- adapter/mux.go
+++ adapter/mux.go
@@ -1,6 +1,7 @@
 package adapter
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net"
@@ -8,6 +9,7 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/oligoden/chassis"
@@ -103,10 +105,10 @@ func (mx *Mux) Serve() {
 		return
 	}
 
-	// if mx.URL.Scheme == "https" {
-	// 	mx.serveTLS()
-	// 	return
-	// }
+	if mx.URL.Scheme == "https" {
+		mx.serveTLS()
+		return
+	}
 }
 
 type grpcError struct {
@@ -154,7 +156,6 @@ func (mx *Mux) serve() {
 				return
 			}
 
-			// profile.RegisterReadProfileServer(grpcServer, dProfile)
 			if err := grpcServer.Serve(lis); err != nil {
 				e := chassis.Mark("failed to serve", err)
 				grpcServerError <- grpcError{i, e}
@@ -192,6 +193,159 @@ func (mx *Mux) serve() {
 	}
 
 	shutdown(httpServer)
+	for _, grpcServer := range mx.GRPCServers {
+		grpcServer.GracefulStop()
+	}
+	time.Sleep(100 * time.Millisecond)
+}
+
+func (mx *Mux) serveTLS() {
+	_, port, err := net.SplitHostPort(mx.URL.Host)
+	if err != nil {
+		fmt.Println(chassis.Mark("getting port to serve", err))
+		os.Exit(1)
+	}
+
+	addr := fmt.Sprintf(":%s", port)
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	httpServer := &http.Server{
+		Addr: ":80",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			host, _, _ := net.SplitHostPort(r.Host)
+			u := r.URL
+			u.Host = net.JoinHostPort(host, ":443")
+			u.Scheme = "https"
+			http.Redirect(w, r, u.String(), http.StatusMovedPermanently)
+		}),
+	}
+
+	tlsConfig := tls.Config{
+		PreferServerCipherSuites: true,
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP521,
+			tls.CurveP384,
+			tls.CurveP256,
+			tls.X25519,
+		},
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			// Best disabled, as they don't provide Forward Secrecy,
+			// but might be necessary for some clients
+			// tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			// tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
+
+	domains := strings.FieldsFunc(os.Getenv("ALLOW"), split)
+	for _, domain := range domains {
+		cert, err := tls.LoadX509KeyPair("certs/certificates/"+domain+".crt", "certs/certificates/"+domain+".key")
+		if err != nil {
+			e := fmt.Errorf("loading certificate for domain %s -> %w", domain, err)
+			fmt.Println(e)
+			os.Exit(1)
+		}
+
+		tlsConfig.Certificates = append(tlsConfig.Certificates, cert)
+	}
+
+	httpsServer := &http.Server{
+		Addr:           ":443",
+		Handler:        mx.Mux,
+		TLSConfig:      &tlsConfig,
+		TLSNextProto:   make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		IdleTimeout:    120 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	serverHTTPError := make(chan error)
+	serverHTTPSError := make(chan error)
+	go func() {
+		err := httpServer.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			serverHTTPError <- err
+			return
+		}
+		fmt.Println("http server shutdown")
+	}()
+	go func() {
+		err := httpsServer.ListenAndServeTLS("", "")
+		if err != nil && err != http.ErrServerClosed {
+			serverHTTPSError <- err
+			return
+		}
+		fmt.Println("https server shutdown")
+	}()
+
+	grpcServerError := make(chan grpcError)
+	for i, grpcServer := range mx.GRPCServers {
+		go func() {
+			lis, err := net.Listen("tcp", fmt.Sprintf(":%d", mx.GRPCPorts[i]))
+			if err != nil {
+				e := chassis.Mark("failed to listen", err)
+				grpcServerError <- grpcError{i, e}
+				return
+			}
+
+			if err := grpcServer.Serve(lis); err != nil {
+				e := chassis.Mark("failed to serve", err)
+				grpcServerError <- grpcError{i, e}
+				return
+			}
+		}()
+	}
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	log.Println("started servers")
+	select {
+	case err := <-serverHTTPError:
+		e := chassis.Mark("http server error", err)
+		fmt.Println(chassis.ErrorTrace(e))
+		shutdown(httpsServer)
+		for _, grpcServer := range mx.GRPCServers {
+			grpcServer.GracefulStop()
+		}
+		time.Sleep(100 * time.Millisecond)
+		os.Exit(1)
+	case err := <-serverHTTPSError:
+		e := chassis.Mark("https server error", err)
+		fmt.Println(chassis.ErrorTrace(e))
+		shutdown(httpServer)
+		for _, grpcServer := range mx.GRPCServers {
+			grpcServer.GracefulStop()
+		}
+		time.Sleep(100 * time.Millisecond)
+		os.Exit(1)
+	case gerr := <-grpcServerError:
+		e := chassis.Mark("grpc server error", gerr.e)
+		fmt.Println(chassis.ErrorTrace(e))
+		shutdown(httpServer)
+		shutdown(httpsServer)
+		for i, grpcServer := range mx.GRPCServers {
+			if i != gerr.i {
+				grpcServer.GracefulStop()
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+		os.Exit(1)
+	case sig := <-quit:
+		fmt.Println("\ngot signal", sig)
+	}
+
+	shutdown(httpServer)
+	shutdown(httpsServer)
 	for _, grpcServer := range mx.GRPCServers {
 		grpcServer.GracefulStop()
 	}
